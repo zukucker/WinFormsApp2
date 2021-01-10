78cc057f86b3e9e9cc8553ad3fc03674c3e51500
zukucker <alexander.froehling@googlemail.com>
2021-01-10 20:30:00 +0100

    with docu

--- Form1.vb
+++ Form1.vb
@@ -2,13 +2,21 @@
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' hole den Dateipfad
         Dim filePath As String = GetImagePath()
 
+        ' lade das Bild
         Dim image1 = Image.FromFile(filePath)
 
+
+        ' erstelle ein Bytearray aus dem oben geladenen Bild
         Dim imageByteArray = ImageToByteArray(image1)
 
+        ' hier kann jetzt eingegriffen werden und das ByteArray in die Datenbank geschrieben
+        ' werden um es später wieder auslesen zu können
 
+
+        ' baue aus dem Bytearray wieder das vollständige Bild und lade es in die PictureBox
         PictureBox1.Image = ByteArrayToImage(imageByteArray)
 
 
@@ -19,17 +27,20 @@ Public Class Form1
         Dim filePath = String.Empty
         Dim result As DialogResult = OpenFileDialog1.ShowDialog()
         Dim ImagePath As String = ""
-        ' Test result.
+
         If result = Windows.Forms.DialogResult.OK Then
 
-            ' Get the file name.
+            ' lese Dateipfad 
             ImagePath = OpenFileDialog1.FileName
 
         End If
 
+        ' gebe Dateipfad zurück
         Return ImagePath
     End Function
 
+
+    'zerlege das bild in bytes und schreibe es in ein array
     Public Function ImageToByteArray(image1 As Image) As Byte()
         Using ms As New MemoryStream()
             image1.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg)
@@ -37,12 +48,12 @@ Public Class Form1
         End Using
     End Function
 
+
+    ' lese das array an bytes und mache daraus wieder ein bild
     Public Function ByteArrayToImage(byteArray As Byte()) As Image
         Using ms = New MemoryStream(byteArray)
             Dim returnImage = Image.FromStream(ms)
-
             Return returnImage
-
         End Using
     End Function
 
